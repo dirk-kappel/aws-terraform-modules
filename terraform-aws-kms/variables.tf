feda0f8aa2621a8999e238f7b382f9fed876908d
Dirk Kappel <dirk.kappel@effectual.com>
2025-02-25 12:56:48 -0700

    updated to dk standards

--- terraform-aws-kms/variables.tf
+++ terraform-aws-kms/variables.tf
@@ -1,10 +1,10 @@
 variable "deletion_window_in_days" {
-  description = <<EOT
-The waiting period, specified in number of days. After the waiting 
-period ends, AWS KMS deletes the KMS key. If you specify a value, 
-it must be between 7 and 30, inclusive. If you do not specify a 
-value, it defaults to 30.
-EOT
+  description = <<-EOT
+    The waiting period, specified in number of days. After the waiting 
+    period ends, AWS KMS deletes the KMS key. If you specify a value, 
+    it must be between 7 and 30, inclusive. If you do not specify a 
+    value, it defaults to 30.
+    EOT
   default     = null
 }
 
@@ -26,12 +26,12 @@ variable "key_alias" {
 }
 
 variable "multi_region" {
-  description = <<EOT
-Specifies whether the KMS key is a multi-Region key. You can create 
-a symmetric or asymmetric multi-Region key, and you can create a 
-multi-Region key with imported key material. You cannot create a 
-multi-Region alias key or a multi-Region key in a custom key store.
-EOT
+  description = <<-EOT
+    Specifies whether the KMS key is a multi-Region key. You can create 
+    a symmetric or asymmetric multi-Region key, and you can create a 
+    multi-Region key with imported key material. You cannot create a 
+    multi-Region alias key or a multi-Region key in a custom key store.
+    EOT
   type        = bool
   default     = null
 }
